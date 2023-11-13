1c63ee83d46a36af7fb61039f3e485090e4b84b0
victorpadilla21 <150448699+victorpadilla21@users.noreply.github.com>
2023-11-13 13:48:54 +0100

    Add files via upload

---
+++ R/sumar.R
@@ -0,0 +1,22 @@
+#' Sum two elements
+#'
+#' @param x First element to sum.
+#' @param y Second element to sum.
+#' @param Negative TRUE if the elements to subtract.
+#'
+#' @return The sum of the two elements.
+#' @export
+#'
+#' @examples
+#' x <- 3
+#' y <- 3
+#' sumar(x,y)
+
+
+sumar <- function(x,y,Negative=FALSE) {
+  z <- x + y
+  if (Negative) {
+    z <- x-y
+  }
+  z
+}
